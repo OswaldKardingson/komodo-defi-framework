8e33f9391845a1e9557223840e2ba95cefc78c45
ca333 <ca333@keemail.me>
2025-12-01 16:11:39 +0100

    update LICENSE IP ownership

--- mm2src/mm2_main/src/rpc.rs
+++ mm2src/mm2_main/src/rpc.rs
@@ -1,5 +1,5 @@
 /******************************************************************************
- * Copyright © 2023 Pampex LTD and TillyHK LTD                                *
+ * Copyright © 2025 Gleec Holding OÜ                                *
  *                                                                            *
  * See the CONTRIBUTOR-LICENSE-AGREEMENT, COPYING, LICENSE-COPYRIGHT-NOTICE   *
  * and DEVELOPER-CERTIFICATE-OF-ORIGIN files in the LEGAL directory in        *
@@ -17,7 +17,7 @@
 //
 //  rpc.rs
 //
-//  Copyright © 2023 Pampex LTD and TillyHK LTD. All rights reserved.
+//  Copyright © 2025 Gleec Holding OÜ. All rights reserved.
 //
 
 use crate::rpc::rate_limiter::RateLimitError;
